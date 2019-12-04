3bbb523091f2b552b454d3079b65b5d88bd6875b
Md Shayon <mdshayon0@gmail.com>
2019-12-04 15:38:02 +0600

    Namespaces, Rooms

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 // https://socket.io/docs/client-api/
 const io = require('socket.io-client');
 
-const socket = io.connect('http://127.0.0.1:3000');
+// const socket = io.connect('http://127.0.0.1:3000');
+const socketgames = io.connect('http://127.0.0.1:3000/games');
 
-socket.on('welcome', (data)=>{
-    console.log("Receved", data);  // when we run node app.js like this it wil give us the message but node server.js need to run
+// socket.on('welcome', (data)=>{
+//     console.log("Receved", data);  // when we run node app.js like this it wil give us the message but node server.js need to run
+//     console.log("new client is connected");
+// });
+
+
+socketgames.on('welcome', (msg)=>{
+    console.log("Receved:", msg); // Namespace Routing from server and showing in client
     
 });
+
+socketgames.emit("joinRoom", "rocket league", );
+
+
+socketgames.on("newUser", res=>console.log(res));
+
+socketgames.on("err", (err)=>{
+    console.log(err);
+});
+socketgames.on("success", (data)=>{
+    console.log(data);
+});
+
--- server.js
+++ server.js
@@ -7,12 +7,33 @@ const http = require('http').createServer();
 const io = require('socket.io')(http);
 
 // https://socket.io/get-started/chat/
+/*
 io.on('connection', (socket)=>{
     socket.emit("welcome", "Hello there welcome to the socket io server");
 });
+*/
 
+
+// ROUTING THOUGH SOCKET
+// https://socket.io/docs/rooms-and-namespaces/
+const gameRooms = ["rocket league", "csgo", 'bt1'];
+io.of('/games').on('connection', (socket)=>{
+    console.log("New client");
+    socket.emit('welcome', "Hello welcome to the games");
+
+    // https://socket.io/docs/rooms-and-namespaces/#Joining-and-leaving
+    socket.on("joinRoom", (room)=>{
+        if(gameRooms.includes(room)){
+            socket.join(room);
+            io.of('/games').in(room).emit('newUser', "new player has joined the room "+ room)
+            socket.emit("success", "You have join to room successfully ")
+        }else{
+            return socket.emit("err", "No Room Names " + room );
+        }
+    })
+});
 
 http.listen(port, ()=>{
     console.log("server is running on 127.0.0.1:"+port);
-    
 }); 
+
